35fbdf19b8b24e7a438d76e97dc78117b384e3f5
M0H1K4N <nejer911@gmail.com>
2024-02-07 21:45:24 +0400

    add

--- Node_1/index.js
+++ Node_1/index.js
@@ -3,18 +3,22 @@ const http = require("http");
 const url = require("url");
 
 const server = http.createServer((req, res) => {
+  const pathName = req.url;
 
-    const pathName = req.url;
-    if(pathName === '/' || pathName === "/overview"){
-        res.end('This is OVERVIEW');
-    } else if(pathName === './product'){
-        res.end('This is Product')
-    }else{
-        res.writeHead(404)
-        res.end("This page is not available")
-    }
-
-  res.end("Hello from The server!");
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is OVERVIEW");
+  } else if (pathName === "./product") {
+    res.end("This is Product");
+  } else if (pathName === "/api") {
+    res.writeHead(404);
+    res.end("API");
+  } else {
+    res.writeHead(404, {
+      "Content-Type": "text/html",
+      "my-own-header": "hello-world",
+    });
+  }
+  res.end("<h1>Page Not Found!</h1>");
 });
 
 server.listen(8000, "127.0.0.1", () => {
