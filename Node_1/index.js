f82b220b0c902458624b30939db28c4738b241d9
M0H1K4N <nejer911@gmail.com>
2024-02-07 16:57:10 +0400

    Listening to the server

--- Node_1/index.js
+++ Node_1/index.js
@@ -1,9 +1,36 @@
 const fs = require('fs')
+const http = require('http')
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
-console.log(textIn)
 
-const textOut = `This is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`
+/////////////////////////////////////////////////////////////
+//Files
 
-fs.writeFileSync('./txt/output.txt', textOut)
-console.log("File written!")
+// Blocking, synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+// console.log(textIn)
+// const textOut = `This is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`
+// fs.writeFileSync('./txt/output.txt', textOut)
+// console.log("File written!")
+
+// Non-blovking, asynchronous way
+// fs.readFile('./txt/start.txt', 'utf-8',  (err, data1) => {
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8',  (err, data2) => {
+//         console.log(data2)
+//     })
+// })
+
+// console.log('Will read file')
+
+
+
+
+//////////////////////////////
+//Server
+
+const server = http.createServer((req, res)=>{
+    res.end("Hello from The server!")
+})
+
+server.listen(8000, '127.0.0.1', () =>{
+    console.log("Listening to request on port 8000")
+})
